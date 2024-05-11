9b2588c5f434a11290faf5c39a44e6a3afe6931a
Stephen <stevegans42@gmail.com>
2024-05-11 12:59:13 -0500

    second

--- park_app/urls.py
+++ park_app/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from . import views
 
-# from .views import (
-#
-#
-# )
-
 urlpatterns = [
     path('', views.home, name='home'),
+    path('parks/', views.parks_view, name='parks'),
 ]
--- park_app/views.py
+++ park_app/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def home(request):
     return render(request, 'park_app/home.html')
+
+
+def parks_view(request):
+    return render(request, 'park_app/parks.html')
+
